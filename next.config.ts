609e07fae8667149eeb00b03804004899b3ba548
bishopans <bishopans@gmail.com>
2025-03-28 14:57:38 -0400

    notailwind

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 export default nextConfig
